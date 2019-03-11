891f670ef441e25494462627514d88351c8df261
unknown <kongxiangwen@homedo.com>
2019-03-11 16:56:34 +0800

    first commit

---
+++ vps_transfer_download.py
@@ -0,0 +1,40 @@
+from flask import Flask, abort, Response, request
+import os
+import requests
+
+app = Flask(__name__)
+app.config["DEBUG"]  = False
+
+file_obj = None
+
+def generate(file_obj):
+    while True:
+        content = file_obj.raw.read(102400)
+        if content:
+            yield content
+        else:
+            break
+
+@app.route('/erha/download/20190131-141414/', methods=['GET'])
+def download_file():
+    try:
+        url = request.args.get('url')
+        filename = os.path.basename(url)
+        file_obj = requests.get(url, stream=True)
+        headers = file_obj.headers
+
+        s = Response(generate(file_obj), content_type=headers.get('Content-Type'))
+
+        for k, v in headers.items():
+            s.headers[k] = v
+        s.headers['Content-Disposition'] = "attachment; filename={}".format(filename)
+        return s
+
+    except Exception as e:
+        if file_obj is not None:
+            file_obj.close()
+        Response.close()
+        abort(404)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
